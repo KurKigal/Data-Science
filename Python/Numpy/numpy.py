7eae3924fa9001ff12bf0928b9d7f53c5054f9f4
Emirhan Keser <keseremirhann@gmail.com>
2024-07-22 16:03:16 +0300

    Update numpy.py
    What and why Numpy

---
+++ Python/Numpy/numpy.py
@@ -0,0 +1,29 @@
+#WHAT IS NUMPY
+'''
+NumPy is the fundamental package for scientific computing in Python. 
+It is a Python library that provides a multidimensional array object, various derived objects (such as masked arrays and matrices), 
+and an assortment of routines for fast operations on arrays, including mathematical, logical, shape manipulation, sorting, selecting, I/O, 
+discrete Fourier transforms, basic linear algebra, basic statistical operations, random simulation and much more.
+'''
+
+#WHY NUMPY
+
+#Powerful N-dimensional arrays
+'''
+Fast and versatile, the NumPy vectorization, indexing, and broadcasting concepts are the de-facto standards of array computing today.'''
+
+#Numerical computing tools
+'''
+NumPy offers comprehensive mathematical functions, random number generators, linear algebra routines, Fourier transforms, and more.'''
+
+#Performant
+'''
+The core of NumPy is well-optimized C code. Enjoy the flexibility of Python with the speed of compiled code.'''
+
+#Interoperable
+'''
+NumPy supports a wide range of hardware and computing platforms, and plays well with distributed, GPU, and sparse array libraries.'''
+
+#Easy to use
+'''
+NumPy's high level syntax makes it accessible and productive for programmers from any background or experience level.'''
